79e7c17651adb343786e93144dbd5d8e084cb108
Andy Guschwan <aguschwan@gmail.com>
2019-02-08 11:50:59 -0600

    org model

--- apps/orgs/models.py
+++ apps/orgs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class OrgManager(models.Manager):
+    pass
+
+
+class Org(models.Model):
+    org_name        =   models.CharField(255)
+    first_name      =   models.CharField(255)
+    last_name       =   models.CharField(255)
+    contact_email   =   models.CharField(255)
+    org_address     =   models.CharField(255)
+    org_city        =   models.CharField(255)
+    org_stage       =   models.CharField(255)
+    password        =   models.CharField(500)
+    created_at      =   models.DateTimeField(auto_now_add)
+    updated_at      =   models.DateTimeField(auto_now)
+
+    objects         =   OrgManager
