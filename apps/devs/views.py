60769e826f605a52966e7cbb7f79e7ff002b4143
Chris Honeycutt <clhoneycutt83@gmail.com>
2019-02-08 00:36:12 -0500

    landing and Success pages complete, registration in progress.

--- apps/devs/views.py
+++ apps/devs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'devs/index.html')
+
+
+def success(request):
+    return render(request, 'devs/success.html')
+
+def devreg(request):
+    return render(request, 'devs/register.html')
+
