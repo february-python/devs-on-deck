9aa113f2f0898243079f1f5a6ec81cb55878739c
Dylan Thierbach <dylanjt15@gmail.com>
2019-02-08 17:39:18 -0800

    Added login/registration routes along with validations. Modified orgs models.

--- apps/orgs/models.py
+++ apps/orgs/models.py
@@ -6,15 +6,15 @@ class OrgManager(models.Manager):
 
 
 class Org(models.Model):
-    org_name        =   models.CharField(255)
-    first_name      =   models.CharField(255)
-    last_name       =   models.CharField(255)
-    contact_email   =   models.CharField(255)
-    org_address     =   models.CharField(255)
-    org_city        =   models.CharField(255)
-    org_stage       =   models.CharField(255)
-    password        =   models.CharField(500)
-    created_at      =   models.DateTimeField(auto_now_add)
-    updated_at      =   models.DateTimeField(auto_now)
+    org_name        =   models.CharField(max_length=255)
+    first_name      =   models.CharField(max_length=255)
+    last_name       =   models.CharField(max_length=255)
+    contact_email   =   models.CharField(max_length=255)
+    org_address     =   models.CharField(max_length=255)
+    org_city        =   models.CharField(max_length=255)
+    org_state       =   models.CharField(max_length=255)
+    password        =   models.CharField(max_length=500)
+    created_at      =   models.DateTimeField(auto_now_add=True)
+    updated_at      =   models.DateTimeField(auto_now=True)
 
-    objects         =   OrgManager
+    objects         =   OrgManager()
