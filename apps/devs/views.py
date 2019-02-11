12b8af3ce12a0626a74f074d4f36c15b23a8991e
Chris Honeycutt <clhoneycutt83@gmail.com>
2019-02-10 20:27:43 -0500

    Completed clickable wireframe for devs app templates

--- apps/devs/views.py
+++ apps/devs/views.py
@@ -1,18 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
 
-def index(request):
-    return render(request, 'devs/index.html')
+def register(req):
+    return render(req, 'devs/register.html')
+
+def create_dev(req):
+    errors = Dev.objects.validate_registration
+    if errors:
+        for error in errors:
+            messages.error(req, error)
+        return redirect('devs:register')
+    dev = Dev.objects.create_dev(req.POST)
+    req.session['dev_id'] = dev.id
+    return redirect('devs:success')
+
+def login(req):
+    return render(req, 'devs/login.html')
+
+def login_dev(req):
+    errors = Dev.objects.validate_login
+    if errors:
+        for error in errors:
+            message.error(req, error)
+        return redirect('devs:login')
+    dev = Dev.objects.get(email=req.POST['email'])
+    req.session['dev_id'] = dev.id
+    req.session['first_name'] = dev.first_name
+    return redirect('devs:success')
 
 
-def success(request):
-    return render(request, 'devs/success.html')
+# added with templates
+def dashboard(req):
+    # if 'dev_id' not in req.session:
+    #     return redirect('devs:index')
+    return render(req, 'devs/dashboard.html')
 
-def devreg(request):
-    return render(request, 'devs/register.html')
+def index(req):
+    return render(req, 'devs/index.html')
 
-def languages(request):
-    return render(request, 'devs/languages.html')
 
-def frameworks(request):
-    return render(request, 'devs/frameworks.html')
+def success(req):
+    return render(req, 'devs/success.html')
+
+def addlanguages(req):
+    # Adds language and bio to DB entry
+    return redirect('devs:frameworks')
+
+def languages(req):
+    return render(req, 'devs/languages.html')
+
+def frameworks(req):
+    return render(req, 'devs/frameworks.html')
+
+def addframeworks(req):
+    # Adds frameworks to DB entry
+    return redirect('devs:success')
+
+def messages(req):
+    return render(req, 'devs/messages.html')
+
+def logout(req):
+    return redirect('devs:index')
 
